98c4601b937b4a9fcc23c620ac939829230a77a3
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2015-10-10 17:56:27 -0700

    Use env variables for config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   uber: {
-    clientId:     '',
-    clientSecret: '',
-    serverToken:  '',
-    redirectUri:  ''
+    clientId:     process.env.UBER_CLIENT_ID,
+    clientSecret: process.env.UBER_CLIENT_SECRET,
+    serverToken:  process.env.UBER_SERVER_TOKEN,
+    redirectUri:  process.env.UBER_REDIRECT_URI
   },
 };
